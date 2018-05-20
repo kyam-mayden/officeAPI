2012a11208a3e9019249c466157b6949fcb01ada
marty-crane <kyam.lh@googlemail.com>
2018-05-20 11:49:06 +0100

    QuoteModelTest added

--- tests/Model/QuoteModelTest.php
+++ tests/Model/QuoteModelTest.php
@@ -2,10 +2,22 @@
 
 
 namespace Tests\Model;
+use PHPUnit\Framework\TestCase;
+use Portal\Model\QuoteModel;
 
 
-
-class QuoteModelTest
+class QuoteModelTest extends TestCase
 {
+    function testConstruct()
+    {
+        $db = $this->createMock(\PDO::class);
+        $case = new QuoteModel($db);
+        $expected = QuoteModel::class;
 
+        $this->assertInstanceOf($expected, $case);
+    }
+
+    function testSuccessGetRandomQuote(){
+        //cannot test as interacts with database
+    }
 }
