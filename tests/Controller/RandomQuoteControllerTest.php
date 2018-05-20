3c2998959706e5445311704c25eeaad3d3525606
marty-crane <kyam.lh@googlemail.com>
2018-05-20 11:52:29 +0100

    RandomQuoteController test added

--- tests/Controller/RandomQuoteControllerTest.php
+++ tests/Controller/RandomQuoteControllerTest.php
@@ -1,10 +1,22 @@
 <?php
 
-
 namespace Tests\Controller;
+use PHPUnit\Framework\TestCase;
+use Portal\Controller\RandomQuoteController;
+use Portal\Model\QuoteModel;
 
-
-class RandomQuoteControllerTest
+class RandomQuoteControllerTest extends TestCase
 {
+    function testConstruct()
+    {
+        $stub = $this->createMock(QuoteModel::class);
 
+        $case = new RandomQuoteController($stub);
+        $expected = RandomQuoteController::class;
+        $this->assertInstanceOf($expected, $case);
+    }
+
+    function testInvoke(){
+        //cannot test as gets data from database
+    }
 }
