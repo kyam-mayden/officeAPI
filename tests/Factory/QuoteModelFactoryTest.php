fb9137bcf4acf577fbcca7567a773b1b0643bd05
marty-crane <kyam.lh@googlemail.com>
2018-05-20 11:33:06 +0100

    QuoteModelFactory test complete

--- tests/Factory/QuoteModelFactoryTest.php
+++ tests/Factory/QuoteModelFactoryTest.php
@@ -1,9 +1,23 @@
 <?php
 
 namespace Tests\Factory;
+use Portal\Factory\QuoteModelFactory;
+use Portal\Model\QuoteModel;
+use Psr\Container\ContainerInterface;
 
 
 class QuoteModelFactoryTest
 {
+    function testInvoke()
+    {
+        $db = $this->createMock(\PDO::class);
+        $container = $this->createMock(ContainerInterface::class);
+        $container->method('get')
+            ->willReturn($db);
 
+        $factory = new QuoteModelFactory;
+        $case = $factory($container);
+        $expected = QuoteModel::class;
+        $this->assertInstanceOf($expected, $case);
+    }
 }
